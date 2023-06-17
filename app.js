56a96fdf990da167dc0c210351a3b98297846077
Sourabh Jyoti Das <dassourabh126@gmail.com>
2023-06-17 11:52:24 +0530

    Add comment

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, "client", 'build')))    // deploy on
 
 
 app.get('/', async (req, res) => {
-   res.sendFile(path.join(__dirname,'client', 'build', 'index.html'))
+   res.sendFile(path.join(__dirname,'client', 'build', 'index.html')) // deploy only
 });
 
 // importing Routes

988a6796da25752a18f379d5548ab8c09d2ab02c
Sourabh Jyoti Das <dassourabh126@gmail.com>
2023-11-29 21:47:56 +0530

    Add backend url

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(fileUpload());
 app.use(
    cors({
      credentials: true,
-     origin: "http://localhost:3000",
+     origin: "https://instalife-in.netlify.app",
      methods: ["GET", "POST", "PUT", "DELETE"],
    })
  );

a131631cc3c5fa20f3ae49327e69d71f42920bbe
Sourabh Jyoti Das <dassourabh126@gmail.com>
2023-11-29 18:36:50 +0530

    Lets Build

--- app.js
+++ app.js
@@ -9,32 +9,27 @@ const app = express();
 dotenv.config({ path: "config/config.env" });
 
 // Using Middlewares
-app.use(cors())
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(fileUpload());
+app.use(
+   cors({
+     credentials: true,
+     origin: "http://localhost:3000",
+     methods: ["GET", "POST", "PUT", "DELETE"],
+   })
+ );
 
 import NodeCache from "node-cache";
 
 export const nodeCache = new NodeCache();
 
-import path from "path";
 
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "./client/build")))    // deploy only
-
-// app.get('/', async (req, res) => {
-//    res.sendFile(path.join(__dirname, './client/build/index.html'));
-// });
-
-app.get('/', (req, res) => {
-   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/', async (req, res) => {
+   res.send("<h1>Working Fine</h1>")
 });
-
-// app.get('/', async (req, res) => {
-//    res.send("<h1>Working Fine</h1>")
-// });
 
 // importing Routes
 import post from "./routes/post.js";

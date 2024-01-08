6761054710c199d11d672790492e0c87ba69cb58
Sourabh Jyoti Das <dassourabh126@gmail.com>
2024-01-08 21:11:22 +0530

    initial

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,20 +1,11 @@
-import User from "../models/User.js";
-import jwt from "jsonwebtoken";
 
 export const isAuthenticated = async (req, res, next) => {
   try {
 
-    const { token } = req.cookies;
     const googletoken = req.cookies["connect.sid"];
 
     if (googletoken) {
       return next()
-    }
-
-    if (token) {
-      const decoded = await jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await User.findById(decoded._id);
-      return next();
     }
 
     return res.status(500).json({

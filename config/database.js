33bfd02c86aa8c35908935edb83733a6fb04a6e3
Sourabh Jyoti Das <dassourabh126@gmail.com>
2024-01-10 20:59:12 +0530

    initial

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectToDatabase = async () => {
    try {
-      const { connection } = await mongoose.connect(process.env.MONGO_URI_LOCAL);
+      const { connection } = await mongoose.connect(process.env.MONGO_URI);
       console.log(`MongoDB connected with ${connection.host}`);
    } catch (error) {
       console.log(error.message);
